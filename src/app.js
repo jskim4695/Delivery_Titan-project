4eba7b91cedbfac8394846e8ec1f0c4a5aa20aac
wanhee27 <chowh727@gmail.com>
2024-02-27 09:34:32 +0900

    Feat: 리뷰 기능 추가

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import UsersRouter from './routes/users.router.js';
 import AuthRouter from './routes/auth.router.js';
 import CartsRouter from './routes/carts.router.js';
 import OrdersRouter from './routes/orders.router.js';
+import ReviewsRouter from './routes/reviews.router.js';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
 import { sessionOption } from './utils/redis/redis.js';
@@ -16,7 +17,13 @@ const PORT = 3000;
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session(sessionOption));
-app.use('/', [UsersRouter, AuthRouter, CartsRouter, OrdersRouter]);
+app.use('/', [
+  UsersRouter,
+  AuthRouter,
+  CartsRouter,
+  OrdersRouter,
+  ReviewsRouter,
+]);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트로 서버가 열렸어요!');
