31da618c5f1bd9dd376af898224fd19b9c9f8217
JY2345 <ilokids@gmail.com>
2024-02-28 12:13:19 +0900

    Test : Test 코드 추가

--- __tests__/unit/repositories/stores.repository.unit.spec.js
+++ __tests__/unit/repositories/stores.repository.unit.spec.js
@@ -1,5 +1,5 @@
 import { jest } from '@jest/globals';
-import { StoresRepository } from '../../src/repositories/stores.repository.js';
+import { StoresRepository } from '../../../src/repositories/stores.repository.js';
 
 let mockPrisma = {
   stores: {
