d6fd4d3600281154e2953c77b02f101b1aa23375
JY2345 <ilokids@gmail.com>
2024-02-28 12:21:16 +0900

    Feat :stores, menus 라우터 추가

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import CartsRouter from './routes/carts.router.js';
 import OrdersRouter from './routes/orders.router.js';
+import StoresRouter from './routes/stores.router.js';
+import menuRouter from './routes/menus.router.js';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
 import { sessionOption } from './utils/redis/redis.js';
@@ -14,8 +16,7 @@ const PORT = 3000;
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session(sessionOption));
-app.use('/', [CartsRouter, OrdersRouter]);
-
+app.use('/', [CartsRouter, OrdersRouter,StoresRouter, menuRouter]);
 app.listen(PORT, () => {
   console.log(PORT, '포트로 서버가 열렸어요!');
 });
